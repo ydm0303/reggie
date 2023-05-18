eadd42846aa9e6d1a5aa160d90c4efa609b4dad5
ydm2 <1139738085@qq.com>
2023-05-19 00:33:16 +0800

    2023年5月19日00点32分-菜品新增

--- src/main/java/com/reggie/service/DishService.java
+++ src/main/java/com/reggie/service/DishService.java
@@ -1,9 +1,16 @@
 package com.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.reggie.dto.DishDto;
 import com.reggie.entity.Dish;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface DishService extends IService<Dish> {
+
+    /**
+     * 新增菜品，同时新增菜品口味，需要同时操作两张表，dish,dishFlavor
+     * @param dishDto
+     */
+    public void saveWithDishFlavor(DishDto dishDto);
 }
--- src/main/java/com/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/reggie/service/impl/DishServiceImpl.java
@@ -2,11 +2,46 @@ package com.reggie.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.reggie.dto.DishDto;
 import com.reggie.entity.Dish;
+import com.reggie.entity.DishFlavor;
 import com.reggie.mapper.DishMapper;
+import com.reggie.service.DishFlavorService;
 import com.reggie.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DishServiceImpl extends ServiceImpl<DishMapper,Dish> implements DishService{
+
+    @Autowired
+    private DishFlavorService dishFlavorService;
+    /**
+     *新增菜品，同时保存菜品口味
+     * @param dishDto
+     */
+    @Transactional  //事务控制，保证数据的一致性
+    public void saveWithDishFlavor(DishDto dishDto) {
+
+        //保存菜品基本信息
+        this.save(dishDto);
+
+        //菜品id
+        Long dishId = dishDto.getId();
+        List<DishFlavor> dishDtoFlavors = dishDto.getFlavors();
+
+        //菜品口味
+        dishDtoFlavors.stream().map((item) ->{
+            item.setDishId(dishId);
+            return item;
+        }).collect(Collectors.toList());
+
+        //保存菜品口味到菜品口味表，dishFlavor
+//        dishFlavorService.saveBatch(dishDto.getFlavors());
+
+    }
 }
