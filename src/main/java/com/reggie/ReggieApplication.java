c4b48bfb1f5aceb2548f77f370d624b438e5534c
ydm <1139738085@qq.com>
2023-06-08 17:26:34 +0800

    修复登录

--- src/main/java/com/reggie/ReggieApplication.java
+++ src/main/java/com/reggie/ReggieApplication.java
@@ -4,13 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
+@EnableScheduling
 @SpringBootApplication
 @ServletComponentScan
+@EnableTransactionManagement  //开启事务注解
+//@ComponentScan(basePackages = "com.reggie.task")
 public class ReggieApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ReggieApplication.class,args);
+        SpringApplication.run(ReggieApplication.class, args);
         log.info("项目启动成功..!");
     }
 }
