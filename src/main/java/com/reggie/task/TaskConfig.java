143adb83ef7e7d71c718bd8ad9a7681f25766dd2
ydm <1139738085@qq.com>
2023-06-08 17:28:52 +0800

    修复登录3

--- src/main/java/com/reggie/task/TaskConfig.java
+++ src/main/java/com/reggie/task/TaskConfig.java
@@ -1,40 +1,40 @@
-package com.reggie.task;
-
-import org.quartz.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-
-@Configuration
-public class TaskConfig {
-
-    @Bean
-    public JobDetailFactoryBean jobDetail(){
-        JobDetailFactoryBean detailFactoryBean = new JobDetailFactoryBean();
-        detailFactoryBean.setJobClass(TestJob.class);
-        detailFactoryBean.setName("这个一个任务名123");
-        detailFactoryBean.setGroup("TestJob");
-        detailFactoryBean.getJobDataMap().put("arms","这是一个参数123");
-        return detailFactoryBean;
-    }
-
-    @Bean
-    public CronTriggerFactoryBean trigger(JobDetail jobDetail123){
-        CronTriggerFactoryBean factoryBean = new CronTriggerFactoryBean();
-        factoryBean.setJobDetail(jobDetail123);
-        //设置延时启动 ms
-        factoryBean.setStartDelay(10000);
-        factoryBean.setCronExpression("0/10 * * * * ?");
-        return factoryBean;
-    }
-
-    @Bean
-    public SchedulerFactoryBean scheduler(Trigger trigger123){
-        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
-        schedulerFactoryBean.setTriggers(trigger123);
-        return schedulerFactoryBean;
-    }
-
-}
+//package com.reggie.task;
+//
+//import org.quartz.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+//import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+//import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+//
+//@Configuration
+//public class TaskConfig {
+//
+//    @Bean
+//    public JobDetailFactoryBean jobDetail(){
+//        JobDetailFactoryBean detailFactoryBean = new JobDetailFactoryBean();
+//        detailFactoryBean.setJobClass(TestJob.class);
+//        detailFactoryBean.setName("这个一个任务名123");
+//        detailFactoryBean.setGroup("TestJob");
+//        detailFactoryBean.getJobDataMap().put("arms","这是一个参数123");
+//        return detailFactoryBean;
+//    }
+//
+//    @Bean
+//    public CronTriggerFactoryBean trigger(JobDetail jobDetail123){
+//        CronTriggerFactoryBean factoryBean = new CronTriggerFactoryBean();
+//        factoryBean.setJobDetail(jobDetail123);
+//        //设置延时启动 ms
+//        factoryBean.setStartDelay(10000);
+//        factoryBean.setCronExpression("0/10 * * * * ?");
+//        return factoryBean;
+//    }
+//
+//    @Bean
+//    public SchedulerFactoryBean scheduler(Trigger trigger123){
+//        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+//        schedulerFactoryBean.setTriggers(trigger123);
+//        return schedulerFactoryBean;
+//    }
+//
+//}
--- src/main/java/com/reggie/task/TestJob.java
+++ src/main/java/com/reggie/task/TestJob.java
@@ -1,29 +1,29 @@
-package com.reggie.task;
-
-import lombok.extern.slf4j.Slf4j;
-import org.quartz.*;
-import org.springframework.scheduling.quartz.QuartzJobBean;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-@Slf4j
-@Component
-public class TestJob extends QuartzJobBean {
-
-
-    @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-
-        JobDataMap jobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
-        String arms = jobDataMap.getString("arms");
-
-        JobDetail jobDetail = jobExecutionContext.getJobDetail();
-        Class<? extends Job> jobClass = jobDetail.getJobClass();
-
-        String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        log.info("TestJob,定时时间：" + dateTime);
-        log.info("arms参数：" + arms );
-    }
-}
+//package com.reggie.task;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.quartz.*;
+//import org.springframework.scheduling.quartz.QuartzJobBean;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//
+//@Slf4j
+//@Component
+//public class TestJob extends QuartzJobBean {
+//
+//
+//    @Override
+//    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+//
+//        JobDataMap jobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+//        String arms = jobDataMap.getString("arms");
+//
+//        JobDetail jobDetail = jobExecutionContext.getJobDetail();
+//        Class<? extends Job> jobClass = jobDetail.getJobClass();
+//
+//        String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+//        log.info("TestJob,定时时间：" + dateTime);
+//        log.info("arms参数：" + arms );
+//    }
+//}
